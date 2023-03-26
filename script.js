8d6a15f593f6cc06888e5830694ebb52f54c49b1
Tarun Kushwaha <116190775+tarunkumarkushwaha@users.noreply.github.com>
2023-03-26 18:40:55 +0530

    input url remove from dom

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 document.querySelector('form').setAttribute("onsubmit", "clickHandler()")
-let iframes = document.querySelectorAll('iframe')
+let iframes = [...document.querySelectorAll('iframe')]
 let searchInput = document.getElementById('fname')
+let iframeremoved = document.createElement('div')
+iframeremoved.innerHTML="Iframe Blocked !!"
 
 const remover = () => {
     let input = localStorage.getItem("searchdata") || "any"
     const start = performance.now();
     for (let i = 0; i < iframes.length; i++) {
-        if (iframes[i].getAttribute("src").split('.').includes(input.toLowerCase())) {
-            iframes[i].setAttribute("style", "display:none")
+        if (iframes[i].getAttribute("src").indexOf((input.toLowerCase())) != -1) {
+            document.body.replaceChild(iframeremoved,iframes[i])
         }
     }
     const end = performance.now();
     console.log(`Execution time: ${end - start} ms`);
 }
-
 document.body.onload = remover()
-
 const clickHandler = () => {
     localStorage.setItem("searchdata", searchInput.value)
 }
