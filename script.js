2376d073cbda6b41d2ab8432a4a711df8b734b28
Tarun Kushwaha <116190775+tarunkumarkushwaha@users.noreply.github.com>
2023-03-26 23:00:00 +0530

    bug in replace child removed

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 document.querySelector('form').setAttribute("onsubmit", "clickHandler()")
-let iframes = [...document.querySelectorAll('iframe')]
+let iframes = document.querySelectorAll('iframe')
 let searchInput = document.getElementById('fname')
-let iframeremoved = document.createElement('div')
-iframeremoved.innerHTML="Iframe Blocked !!"
 
 const remover = () => {
     let input = localStorage.getItem("searchdata") || "any"
     const start = performance.now();
     for (let i = 0; i < iframes.length; i++) {
         if (iframes[i].getAttribute("src").indexOf((input.toLowerCase())) != -1) {
-            document.body.replaceChild(iframeremoved,iframes[i])
+            let iframeremoved = document.createElement('div')
+            iframeremoved.innerHTML = "Iframe Blocked !!"
+            document.body.replaceChild(iframeremoved, iframes[i])
         }
     }
     const end = performance.now();
