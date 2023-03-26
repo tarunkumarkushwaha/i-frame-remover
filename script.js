8a7d150ee81a47864b4a808b9bed70950d2b4a79
Tarun Kushwaha <116190775+tarunkumarkushwaha@users.noreply.github.com>
2023-03-26 15:55:36 +0530

    logic changed

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 document.querySelector('form').setAttribute("onsubmit", "clickHandler()")
 let iframes = document.querySelectorAll('iframe')
 let searchInput = document.getElementById('fname')
+
 const remover = () => {
     let input = localStorage.getItem("searchdata") || "any"
     const start = performance.now();
     for (let i = 0; i < iframes.length; i++) {
-        const filtered = Object.values(iframes[i].attributes)
-        if (filtered.find(o => o.name === 'src').value.split('.').includes(input.toLowerCase())) {
-            filtered, iframes[i].setAttribute("style", "display:none")
+        if (iframes[i].getAttribute("src").split('.').includes(input.toLowerCase())) {
+            iframes[i].setAttribute("style", "display:none")
         }
     }
     const end = performance.now();
     console.log(`Execution time: ${end - start} ms`);
 }
+
 document.body.onload = remover()
 
 const clickHandler = () => {
     localStorage.setItem("searchdata", searchInput.value)
 }
-// onsubmit="clickHandler()"
