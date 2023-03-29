7796f2e068c43aea45938278f42b7686f80d7cfc
Tarun Kushwaha <116190775+tarunkumarkushwaha@users.noreply.github.com>
2023-03-29 23:20:58 +0530

    iframe blocker, script unblocker

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
-document.querySelector('form').setAttribute("onsubmit", "clickHandler()")
-let iframes = document.querySelectorAll('iframe')
-let searchInput = document.getElementById('fname')
-
-const remover = () => {
-    let input = localStorage.getItem("searchdata") || "any"
-    for (let i = 0; i < iframes.length; i++) {
-        if (iframes[i].getAttribute("src").indexOf((input.toLowerCase())) != -1) {
+const iframeblocker = () => {
+    if (confirm("do you want to remove iframes")) {
+        let getiframes = document.querySelectorAll('iframe')
+        for (let i = 0; i < getiframes.length; i++) {
             let iframeremoved = document.createElement('div')
             iframeremoved.innerHTML = "Iframe Blocked !!"
-            document.body.replaceChild(iframeremoved, iframes[i])
+            document.body.replaceChild(iframeremoved, getiframes[i])
+        }
+
+    }
+}
+iframeblocker()
+
+const scriptunblocker = () => {
+    if (confirm("do you want to enable scripts")) {
+        let getscript = document.querySelectorAll('script[cz-script-category]')
+        for (let i = 0; i < getscript.length; i++) {
+            if (getscript[i].getAttribute('cz-script-category').trim() == "functional") {
+                getscript[i].setAttribute("type", "text/javascript")
+            }
+
         }
     }
 }
-document.body.onload = remover()
-const clickHandler = () => {
-    localStorage.setItem("searchdata", searchInput.value)
-}
+
+// scriptunblocker()
+
+
+
+
+
+
+// document.querySelector('form').setAttribute("onsubmit", "clickHandler()")
+// let iframes = document.querySelectorAll('iframe')
+// let searchInput = document.getElementById('fname')
+
+// const remover = () => {
+//     let input = localStorage.getItem("searchdata") || "any"
+//     for (let i = 0; i < iframes.length; i++) {
+//         if (iframes[i].getAttribute("src").indexOf((input.toLowerCase())) != -1) {
+//             let iframeremoved = document.createElement('div')
+//             iframeremoved.innerHTML = "Iframe Blocked !!"
+//             document.body.replaceChild(iframeremoved, iframes[i])
+//         }
+//     }
+// }
+// // document.body.onload = remover()
+// const clickHandler = () => {
+//     localStorage.setItem("searchdata", searchInput.value)
+// }
